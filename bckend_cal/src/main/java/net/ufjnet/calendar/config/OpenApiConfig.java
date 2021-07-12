83b7137da7e3250eec5ba3ae9dbb070fd33efc56
GPMS <gab.portela@hotmail.com>
2021-07-12 15:59:11 -0300

    Add Swagger support

---
+++ bckend_cal/src/main/java/net/ufjnet/calendar/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package net.ufjnet.calendar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+
+	@Bean
+	public OpenAPI customOpenApi() {
+		return new OpenAPI()
+				.info(new Info()
+				.title("API RESTful Calendar built with Spring Boot 2.5.2")
+				.version("V1")
+				.description("Simple Calendar APP")
+				.termsOfService("http://swagger.io/terms")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+			
+	}
+
+	
+}
--- bckend_cal/src/main/java/net/ufjnet/calendar/controllers/UserController.java
+++ bckend_cal/src/main/java/net/ufjnet/calendar/controllers/UserController.java
@@ -1,7 +1,5 @@
 package net.ufjnet.calendar.controllers;
 
-import javax.validation.Valid;
-
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -15,28 +13,32 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import net.ufjnet.calendar.dtos.UserDTO;
 import net.ufjnet.calendar.models.User;
 import net.ufjnet.calendar.services.UserService;
 
 @RestController
 @RequestMapping("/v1/cal/users")
+@Tag(name = "User's Endpoint")
 public class UserController {
 
 	@Autowired
 	private UserService service;
 	
 	@GetMapping
+	@Operation(summary = "Finds all users")
 	public ResponseEntity<CollectionModel<UserDTO>> Findall(@RequestParam(value = "page", defaultValue = "0") int page,
 															@RequestParam(value = "limit", defaultValue = "12") int limit,
 															@RequestParam(value = "direction", defaultValue = "asc") String direction) {
@@ -57,6 +59,7 @@ public class UserController {
 	}
 
 	@GetMapping("/{id}")
+	@Operation(summary = "Finds one user given his id")
 	public ResponseEntity<UserDTO> FindOne(@PathVariable Integer id) {
 		UserDTO objDTO = service.FindByID(id);
 		objDTO.add(linkTo(methodOn(UserController.class).FindOne(id)).withSelfRel());
@@ -64,6 +67,7 @@ public class UserController {
 	}
 
 	@GetMapping("/name/{name}")
+	@Operation(summary = "Finds one user given his name")
 	public ResponseEntity<UserDTO> FindName(@PathVariable String name) {
 		UserDTO objDTO = service.FindByName(name);
 		objDTO.add(linkTo(methodOn(UserController.class).FindName(name)).withSelfRel());
@@ -71,6 +75,7 @@ public class UserController {
 	}
 
 	@GetMapping("/email/{email}")
+	@Operation(summary = "Finds one user given his e-mail")
 	public ResponseEntity<UserDTO> FindEmail(@PathVariable String email) {
 		UserDTO objDTO = service.FindByEmail(email);
 		objDTO.add(linkTo(methodOn(UserController.class).FindEmail(email)).withSelfRel());
@@ -79,6 +84,7 @@ public class UserController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
+	@Operation(summary = "Inserts a new user in the Database")
 	public ResponseEntity<UserDTO> Save(@RequestBody User objBody) {
 		UserDTO objDTO = service.Save(objBody);
 		objDTO.add(linkTo(methodOn(UserController.class).FindOne(objDTO.getId())).withSelfRel());
@@ -86,6 +92,7 @@ public class UserController {
 	}
 
 	@PutMapping("/{id}")
+	@Operation(summary = "Updates one user given his id")
 	public ResponseEntity<UserDTO> Update(@PathVariable Integer id, @RequestBody User objBody) {
 		if (!service.ExistsByID(id)) {
 			return ResponseEntity.notFound().build();
@@ -97,6 +104,7 @@ public class UserController {
 	}		
 
 	@DeleteMapping("/{id}")
+	@Operation(summary = "Deletes one user given his id")
 	public ResponseEntity<Void> Delete(@PathVariable Integer id) {
 		if (!service.ExistsByID(id)) {
 			return ResponseEntity.notFound().build();
