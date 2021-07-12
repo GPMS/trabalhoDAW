10bfe20976b60bed0c492366c84f96afe0d17222
GPMS <gab.portela@hotmail.com>
2021-07-12 11:15:17 -0300

    Add 3 sample users for testing

--- bckend_cal/src/main/java/net/ufjnet/calendar/BckendCalApplication.java
+++ bckend_cal/src/main/java/net/ufjnet/calendar/BckendCalApplication.java
@@ -1,13 +1,31 @@
 package net.ufjnet.calendar;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import net.ufjnet.calendar.models.User;
+import net.ufjnet.calendar.repositories.UserDAO;
 
 @SpringBootApplication
-public class BckendCalApplication {
+public class BckendCalApplication implements CommandLineRunner {
+	
+	@Autowired
+	private UserDAO userDAO;	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BckendCalApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		User u1 = new User(1, "Gabriel", "gabriel@cal");
+		User u2 = new User(2, "Gabrieli", "gabrieli@cal");
+		User u3 = new User(3, "Roberto", "roberto@cal");
+		
+		userDAO.saveAll(Arrays.asList(u1, u2, u3));
 	}
 }
