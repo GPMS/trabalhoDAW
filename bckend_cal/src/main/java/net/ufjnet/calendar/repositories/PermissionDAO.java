658f825fd3537d52af1ad9289484a3b727692623
GPMS <gab.portela@hotmail.com>
2021-08-26 13:54:50 -0300

    Remove useless imports

--- bckend_cal/src/main/java/net/ufjnet/calendar/repositories/PermissionDAO.java
+++ bckend_cal/src/main/java/net/ufjnet/calendar/repositories/PermissionDAO.java
@@ -1,14 +1,7 @@
 package net.ufjnet.calendar.repositories;
 
-import java.util.Optional;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import net.ufjnet.calendar.models.Category;
 import net.ufjnet.calendar.models.Permission;
 
 public interface PermissionDAO extends JpaRepository<Permission, Integer> {
