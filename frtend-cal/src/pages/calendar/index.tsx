a7ae2860741fe069a1cc6b7c8c10248ca6e03fcd
GPMS <gab.portela@hotmail.com>
2021-09-21 10:52:36 -0300

    Front-End: Add calendar page

--- frtend-cal/src/pages/calendar/index.tsx
+++ frtend-cal/src/pages/calendar/index.tsx
@@ -1,7 +1,16 @@
-export default function Calendar() {
-  return (
-    <>
-    <h1>Calendar</h1>
-    </>
-  );
+import React from 'react'
+import { Inject, ScheduleComponent, Day, Week, Month, Agenda,
+         EventSettingsModel } from '@syncfusion/ej2-react-schedule';
+
+export default class Calendar extends React.Component {
+  private localData: EventSettingsModel = {
+    dataSource: [{
+    }]
+  };
+
+  public render() {
+    return <ScheduleComponent currentView='Month' eventSettings={this.localData}>
+      <Inject services={[Day, Week, Month, Agenda]} />
+    </ScheduleComponent>
+  }
 }
